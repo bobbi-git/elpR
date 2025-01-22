0ab10997898e7668f41b3189c1278c9078303b9e
bobbi-git <83470805+bobbi-git@users.noreply.github.com>
2025-01-22 17:51:11 -0500

    updates to selection table merge
    Added details for description of function

--- R/elpR_SelectionTable_Merge-General_function.R
+++ R/elpR_SelectionTable_Merge-General_function.R
@@ -3,6 +3,15 @@
 # bje37@cornell.edu
 # Updated Jan 2025
 
+# This function will
+# - merge selection tables that are in a single folder or subfolders
+# - selection tables can have differing columns and column order - the script will standardize across tables
+# - this script will merge all selections into one table and will rename the Selection ID so they are all unique
+# - This script expects Raven Sound Selection Tables with columns: Selection, Begin Time (s),
+#        End Time (s), High Freq (s), Low Freq (s), File Offset (s), Begin File, Begin Path
+# - output file is .txt selection table
+
+
 
 merge_selection_tables <- function(path, recursive = TRUE) {
   # Check if path exists
@@ -104,7 +113,7 @@ merge_selection_tables <- function(path, recursive = TRUE) {
 
     # Remove duplicates
     merged_df <- merged_df %>%
-      distinct(`Begin File`, `File Offset (s)`, .keep_all = TRUE)
+      distinct(`Begin File`, `File Offset (s)`,`End Time (s)`, `High Freq (Hz)`,`Low Freq (Hz)`, .keep_all = TRUE)
   }
 
   # Sort by Begin File and File Offset
--- Scripts/elpR_SelectionTable_Merge-General.R
+++ Scripts/elpR_SelectionTable_Merge-General.R
@@ -3,9 +3,16 @@
 # bje37@cornell.edu
 # Updated Jan 2025
 
+# This function will
+# - merge selection tables that are in a single folder or subfolders
+# - selection tables can have differing columns and column order - the script will standardize across tables
+# - this script will merge all selections into one table and will rename the Selection ID so they are all unique
+# - This script expects Raven Sound Selection Tables with columns: Selection, Begin Time (s),
+#        End Time (s), High Freq (s), Low Freq (s), File Offset (s), Begin File, Begin Path
+# - output file is .txt selection table
+
 
 path <- "C:/Users/bje37/Documents/R/Bobbi_Scripts/Packages/elpR/Files/temp" # directory where selection tables are saved (can have subfolders)
-
 
 # For files in a single folder:
 merge_selection_tables(path, recursive = FALSE)
